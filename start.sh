5e8daf47e7f7396cdba09f4ea8a2386a21ba7ba0
Appu <99067787+chriswinsa@users.noreply.github.com>
2022-04-12 11:40:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
 git clone -b master https://github.com/chriswinsa/mohalal.git /mohalal
-cd /tgmoviebot
+cd /mohalal
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
